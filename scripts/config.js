910741403a89666f1e29e81bba310ca88303252f
yunnysunny <yunnysunny@users.noreply.github.com>
2022-04-01 03:53:43 +0000

    deploy: fcbca786d1b18ccd599603cfdba44b7b6501060b

--- scripts/config.js
+++ scripts/config.js
@@ -2,7 +2,8 @@ const app_id = '6243e61458b3c60aa1ec0e7d';
 const app_name = 'aws-demo-yunnysunny';
 const sp_redirect_url = 'https://yunnysunny.github.io/aws-authing-demo';
 
-const aws_api_id = 'sy7yaxw86d';
+const aws_lambda_auth_api_id = 'sy7yaxw86d';
+const aws_oidc_auth_api_id = '';
 const region = 'cn-north-1';
 const aws_identity_pool_id = 'cn-north-1:20716676-b43c-46d5-a30e-c47aff75ef3c';
 const aws_bucket_name_test = 'get-test';
